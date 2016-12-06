8107750a4abe58eace5075d71867111bf39b7b4f
Magiczne <michalkamilkleszczynski@gmail.com>
2016-12-06 15:52:46 +0100

    Day 5(One Star)

---
+++ AdventOfCode/Day 5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day_5
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var solution = new Solution();
+            solution.Solve();
+
+            Console.Read();
+        }
+    }
+
+    internal class Solution
+    {
+        private const string Input = "cxdnnyjw";
+
+        public void Solve()
+        {
+            var password = string.Empty;
+
+            for (var i = 0; password.Length < 8; i++)
+            {
+                var encoded = Util.HashMd5(Input + i);
+
+                if (encoded.Substring(0, 5) == "00000")
+                {
+                    password += encoded[5];
+                }
+            }
+
+            Console.WriteLine("*: " + password);
+        }
+    }
+}
---
+++ AdventOfCode/Day 5/Util.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Day_5
+{
+    internal static class Util
+    {
+        public static string HashMd5(string input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var inputBytes = Encoding.ASCII.GetBytes(input);
+                var hashBytes = md5.ComputeHash(inputBytes);
+
+                var sb = new StringBuilder();
+
+                foreach(var b in hashBytes)
+                {
+                    sb.Append(b.ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}

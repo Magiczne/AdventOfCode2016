5c1c421a89b2009bcd4e8d94e09c10e27dd90d35
Magiczne <michalkamilkleszczynski@gmail.com>
2016-12-06 12:27:41 +0100

    Day 4(One Star)

---
+++ AdventOfCode/Day 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_4
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var solution = new Solution();
+            solution.Solve();
+
+            Console.Read();
+        }
+    }
+
+    internal class Solution
+    {
+        private readonly List<Room> _rooms = new List<Room>();
+
+        public void Solve()
+        {
+            GetInput();
+
+            var sectorSum = _rooms.Where(room => room.IsValid).Sum(room => room.SectorId);
+
+            Console.WriteLine("Answers: ");
+            Console.WriteLine("*: " + sectorSum);
+        }
+
+        private void GetInput()
+        {
+            var data = File.ReadAllLines("../../input.txt");
+
+            foreach (var line in data)
+            {
+                _rooms.Add(new Room(line));
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Day 4/Room.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day_4
+{
+    internal class Room
+    {
+        private readonly List<string> _encryptedName = new List<string>();
+        private string _checksum;
+
+        private readonly Dictionary<char, int> _lettersFreq = new Dictionary<char, int>();
+
+        public int SectorId { get; private set; }
+
+        public bool IsValid
+        {
+            get
+            {
+                var mostCommon = _lettersFreq.OrderByDescending(p => p.Value)
+                            .ThenBy(p => p.Key)
+                            .Select(p => p.Key).Take(_checksum.Length).ToArray();
+
+                var checkSum = new string(mostCommon);
+
+                return checkSum == _checksum;
+            }
+        }
+        public Room(string line)
+        {
+            GetData(line);
+
+            foreach (var part in _encryptedName)
+            {
+                foreach (var c in part)
+                {
+                    if (_lettersFreq.ContainsKey(c))
+                    {
+                        _lettersFreq[c]++;
+                    }
+                    else
+                    {
+                        _lettersFreq[c] = 1;
+                    }
+                }               
+            }
+        }
+
+        private void GetData(string line)
+        {
+            line = line.Trim();
+            var regex = new Regex(@"([a-z]+)[-]+?(\d+)?([[]([a-z]+)[]])?");
+
+            var matches = regex.Matches(line);
+
+            for (var i = 0; i < matches.Count; i++)
+            {
+                if (i != matches.Count - 1)
+                {
+                    _encryptedName.Add(matches[i].Groups[1].ToString());
+                }
+                else
+                {
+                    _encryptedName.Add(matches[i].Groups[1].ToString());
+                    SectorId = int.Parse(matches[i].Groups[2].ToString());
+                    _checksum = matches[i].Groups[4].ToString();
+                }
+            }
+        }
+    }
+}

0d79d2fc3897295fe613dbbcabbd182b6b51117f
Magiczne <michalkamilkleszczynski@gmail.com>
2016-12-07 21:56:04 +0100

    Some cleanup.

---
+++ AdventOfCode/Day 7/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Day_7
+{
+    internal static class StringExtensions
+    {
+        public static bool HasAbba(this string s)
+        {
+            if (s.Length < 4) return false;
+
+            for (var i = 0; i < s.Length - 3; i++)
+            {
+                if (s[i] == s[i + 3])
+                {
+                    if (s[i + 1] == s[i + 2] && s[i] != s[i + 1])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}

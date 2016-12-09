8433f57f7bd42fd590d09412cb58ae675f80285f
Magiczne <michalkamilkleszczynski@gmail.com>
2016-12-09 19:10:12 +0100

    Input files added to Day 1 and Day 2. Some Cleanup.

--- AdventOfCode/Day 1/Program.cs
+++ AdventOfCode/Day 1/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace AdventOfCode
 {
     internal static class Program
     {
-        private const string Input =
-            "L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4";
+        private static string _input;
 
         private static Direction _currentDirection = Direction.North;
 
@@ -20,7 +20,9 @@ namespace AdventOfCode
 
         private static void Main()
         {
-            var instructions = Input.Split(new[] {", "}, StringSplitOptions.None);
+            _input = File.ReadAllText("../../input.txt");
+
+            var instructions = _input.Split(new[] {", "}, StringSplitOptions.None);
 
             foreach (var i in instructions)
             {

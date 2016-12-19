57fc1f68c71bcf3bcd5f186c19133a2119df4f20
Magiczne <michalkamilkleszczynski@gmail.com>
2016-12-19 20:24:26 +0100

    Day 10(One Star)

---
+++ AdventOfCode/Day 10/Bot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day_10
+{
+    internal partial class Solution
+    {
+        private class Bot
+        {
+            private readonly int?[] _chips = {null, null};
+
+            public Bot(int id, int high, int low, bool highOutput, bool lowOutput)
+            {
+                Id = id;
+                High = high;
+                Low = low;
+                HighOutput = highOutput;
+                LowOutput = lowOutput;
+            }
+
+            public int Id { get; }
+
+            private int High { get; }
+            private int Low { get; }
+
+            private bool HighOutput { get; }
+            private bool LowOutput { get; }
+
+            public void GiveChip(int chip)
+            {
+                if (!_chips[0].HasValue) _chips[0] = chip;
+                else if (!_chips[1].HasValue) _chips[1] = chip;
+            }
+
+            public void Process()
+            {
+                if (!_chips[0].HasValue || !_chips[1].HasValue) return;
+
+                //One Star
+                if (_chips.Contains(61) && _chips.Contains(17))
+                    Console.WriteLine("*: Bot " + Id);
+
+                int high;
+                int low;
+
+                if (_chips[0] > _chips[1])
+                {
+                    high = _chips[0].Value;
+                    low = _chips[1].Value;
+                }
+                else
+                {
+                    high = _chips[1].Value;
+                    low = _chips[0].Value;
+                }
+                _chips[0] = null;
+                _chips[1] = null;
+
+                if (HighOutput)
+                {
+                    Outputs[High] = high;
+                }
+                else
+                {
+                    var highBot = Bots.Find(b => b.Id == High);
+                    highBot.GiveChip(high);
+                    highBot.Process();
+                }
+
+                if (LowOutput)
+                {
+                    Outputs[Low] = low;
+                }
+                else
+                {
+                    var lowBot = Bots.Find(b => b.Id == Low);
+                    lowBot.GiveChip(low);
+                    lowBot.Process();
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Day 10/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day_10
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var solution = new Solution();
+            solution.Solve();
+
+            Console.Read();
+        }
+    }
+
+    internal partial class Solution
+    {
+        private readonly List<string> _data;
+        private static readonly List<Bot> Bots = new List<Bot>();
+        private static readonly Dictionary<int, int> Outputs = new Dictionary<int, int>();
+
+        public Solution()
+        {
+            _data = File.ReadAllLines("../../input.txt").ToList();
+            _data.Sort();
+        }
+
+        public void Solve()
+        {
+            var regex = new Regex(@"value (?<val>\d+) goes to bot (?<bot>\d+)|bot (?<s_bot>\d+) gives low to (?<low_to>bot|output) (?<low_value>\d+) and high to (?<high_to>bot|output) (?<high_value>\d+)");
+
+            foreach (var line in _data)
+            {
+                var match = regex.Match(line);
+
+                //bot x gives low to ...
+                if (match.Groups["s_bot"].Success)
+                {
+                    var id = int.Parse(match.Groups["s_bot"].ToString());
+                    var high = int.Parse(match.Groups["high_value"].ToString());
+                    var low = int.Parse(match.Groups["low_value"].ToString());
+                    var highOutput = match.Groups["high_to"].ToString() == "output";
+                    var lowOutput = match.Groups["low_to"].ToString() == "output";
+
+                    var bot = new Bot(id, high, low, highOutput, lowOutput);
+                    Bots.Add(bot);
+                }
+                //value x goest to bot y
+                else
+                {
+                    var val = int.Parse(match.Groups["val"].ToString());
+                    var id = int.Parse(match.Groups["bot"].ToString());
+
+                    var bot = Bots.Find(b => b.Id == id);
+                    bot.GiveChip(val);
+                    bot.Process();
+                }
+            }
+        }
+    }
+}

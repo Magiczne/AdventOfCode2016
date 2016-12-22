640b9e1e1302cdffda964a59265e0b6fd2ab3d72
Magiczne <michalkamilkleszczynski@gmail.com>
2016-12-22 18:37:51 +0100

    Day 12(One Star)

---
+++ AdventOfCode/Day 12/Command.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day_12
+{
+    internal partial class Solution
+    {
+        private class Command
+        {
+            private readonly string _type;
+            private readonly string[] _params = new string[2];
+
+            public Command(string line)
+            {
+                var data = line.Split(' ');
+
+                _type = data[0];
+                _params[0] = data[1];
+                if (_type == "cpy" || _type == "jnz")
+                {
+                    _params[1] = data[2];
+                }
+            }
+
+            public int Exec()
+            {
+                switch (_type)
+                {
+                    case "cpy":
+                    {
+                        var value = Regex.IsMatch(_params[0], @"^-?\d+$")
+                            ? int.Parse(_params[0])
+                            : Registers[_params[0]];
+
+                        Registers[_params[1]] = value;
+                        break;
+                    }
+
+                    case "inc":
+                        Registers[_params[0]]++;
+                        break;
+
+                    case "dec":
+                        Registers[_params[0]]--;
+                        break;
+
+                    case "jnz":
+                    {
+                        var value = Regex.IsMatch(_params[0], @"^-?\d+$")
+                            ? int.Parse(_params[0])
+                            : Registers[_params[0]];
+
+                        if (value != 0)
+                        {
+                            return int.Parse(_params[1]);
+                        }
+
+                        break;
+                    }
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                return 1;
+            }
+        }
+    }
+}
+//
---
+++ AdventOfCode/Day 12/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_12
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Answers: ");
+
+            var solution = new Solution();
+            solution.Solve();
+
+            Console.Read();
+        }
+    }
+
+    internal partial class Solution
+    {
+        private static readonly Dictionary<string, int> Registers = new Dictionary<string, int>
+        {
+            {"a", 0},
+            {"b", 0},
+            {"c", 0},
+            {"d", 0}
+        };
+
+        private readonly List<Command> _commands = new List<Command>();
+
+        public Solution()
+        {
+            var lines = File.ReadAllLines("../../input.txt");
+            foreach (var t in lines)
+            {
+                _commands.Add(new Command(t.Trim()));
+            }
+        }
+
+        public void Solve()
+        {
+            for (var i = 0; i < _commands.Count;)
+            {
+                var tmp = i + _commands[i].Exec();
+
+                if (tmp < 0) tmp = i;
+                if (tmp >= _commands.Count) break;
+
+                i = tmp;
+            }
+
+            Console.WriteLine("*: " + Registers["a"]);
+        }
+    }
+}
